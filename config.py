59ddff47f55a9d173605c2c4db08920c8c6c42e4
s373r <s373r@users.noreply.github.com>
2017-09-16 12:07:35 +0300

    made "copy_config_files" function

--- config.py
+++ config.py
@@ -6,6 +6,30 @@ import os
 import shutil
 
 
+def copy_config_files(source_dir_path, destination_dir_path):
+    for file in files:
+        source_file_path = source_dir_path / file
+        destination_file_path = destination_dir_path / file
+
+        source_file = str(source_file_path)
+        destination_file = str(destination_file_path)
+
+        destination_dir = str(destination_dir_path)
+
+        if not source_file_path.exists():
+            raise FileNotFoundError(source_file)
+
+        # todo add a cool colored logger
+        print('from', source_file, 'to', destination_file)
+
+        os.makedirs(destination_dir, exist_ok=True)
+
+        if destination_file_path.exists():
+            os.remove(destination_file)
+
+        shutil.copy(source_file, destination_file)
+
+
 # todo make install by symlinks, not copying config files
 parser = argparse.ArgumentParser()
 parser.add_argument('action',
@@ -28,30 +52,6 @@ dotfile_repo_path = Path(__file__).absolute().parent
 
 # todo refactoring: move to policy-based paradigm
 if script_action == 'grub':
-    source_dir_path = home_path
-    destination_dir_path = dotfile_repo_path
+    copy_config_files(home_path, dotfile_repo_path)
 else:
-    source_dir_path = dotfile_repo_path
-    destination_dir_path = home_path
-
-for file in files:
-    source_file_path = source_dir_path / file
-    destination_file_path = destination_dir_path / file
-
-    source_file = str(source_file_path)
-    destination_file = str(destination_file_path)
-
-    destination_dir = str(destination_dir_path)
-
-    if not source_file_path.exists():
-        raise FileNotFoundError(source_file)
-
-    # todo add a cool colored logger
-    print('from', source_file, 'to', destination_file)
-
-    os.makedirs(destination_dir, exist_ok=True)
-
-    if destination_file_path.exists():
-        os.remove(destination_file)
-
-    shutil.copy(source_file, destination_file)
+    copy_config_files(dotfile_repo_path, home_path)
