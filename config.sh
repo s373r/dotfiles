edf1056bf37bf1efd3ced900512f453066f19ad4
s373r <you@example.com>
2018-06-11 13:20:56 +0300

    Added new config install workflow via config.sh
    * added fresh .zshrc

---
+++ config.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/env bash
+
+set -e;
+
+DOTFILES="$(dirname "$0")";
+
+function echo_to_stderr() {
+	echo "$*" 1>&2;
+}
+
+function die() {
+	echo_to_stderr "$*";
+	exit 1;
+}
+
+function failure() {
+	local lineNo=$1;
+	echo_to_stderr "Failed at $lineNo line";
+}
+
+trap 'failure ${LINENO}' ERR
+
+function symlinkHasPresent() {
+	# todo check args
+	local what="${1}";
+
+	local source="${DOTFILES}/home/${what}";
+	local symlink="${HOME}/${what}";
+
+	if [ ! -f "${source}" ]; then
+		die "Error while creating a symlink: \"${source}\" not found!";
+	fi
+
+	if [ -L "${symlink}" ]; then
+		echo "Skipping creation of a symlink for \"${what}\: already exists.";
+		return 0;
+	fi
+	
+	if [ -f "${symlink}" ]; then
+		echo "Backuping existed file: \"${what}\"";
+		mv "${symlink}" "${symlink}.backup"
+	fi
+
+	echo "Creation a symlink for \"${what}\"";
+	ln -s "${source}" "${symlink}";
+}
+
+function doInstall() {
+	symlinkHasPresent .bashrc;
+}
+
+function main() {
+	local mode="${1}";
+
+	case "${mode}" in
+		"install")
+			doInstall;
+			;;
+		*)
+			die "Mode \"${mode}\" isn't supported. Supported modes: install"
+			;;
+	esac
+}
+
+
+main "${1}";
